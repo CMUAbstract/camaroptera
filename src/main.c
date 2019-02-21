87d8de5cb8e4f4ca29c1bc3daa4ed4024e524a06
desaiharsh <harshd@andrew.cmu.edu>
2019-02-21 17:55:52 -0500

    Added submodules

---
+++ src/main.c
@@ -0,0 +1,157 @@
+#include <msp430.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include <liblora/mcu.h>
+#include <liblora/uart.h>
+#include <liblora/spi.h>
+#include <liblora/sx1276.h>
+#include <liblora/sx1276regs-fsk.h>
+#include <liblora/sx1276regs-lora.h>
+
+#include <libio/console.h>
+
+#define RF_FREQUENCY   915000000 // Hz
+
+#define FSK_FDEV                          25e3      // Hz
+#define FSK_DATARATE                      50e3      // bps
+#define FSK_BANDWIDTH                     50e3      // Hz
+#define FSK_AFC_BANDWIDTH                 83.333e3  // Hz
+#define FSK_PREAMBLE_LENGTH               5         // Same for Tx and Rx
+#define FSK_FIX_LENGTH_PAYLOAD_ON         false
+
+#define LORA_BANDWIDTH                              2        // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
+#define LORA_SPREADING_FACTOR                       7        // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         5         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+#define RX_TIMEOUT_VALUE                  3500
+#define TX_OUTPUT_POWER                   7        // dBm
+#define BUFFER_SIZE                       256 // Define the payload size here
+
+uint8_t buffer[BUFFER_SIZE];
+char temp[30];
+
+static radio_events_t radio_events;
+
+int state = 0;
+
+void SendPing() {
+   sx1276_send(buffer, 5);
+}
+
+void OnTxDone() {
+ // PRINTF("$TXS\n");
+  //if(state == 1) sx1276_set_rx(0);
+	P4OUT &= ~BIT1;
+}
+
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr) {
+  PRINTF("Packet Received.\r\n");
+
+	PRINTF("RSSI: ");
+	sprintf( temp, "%d", rssi);
+	PRINTF(temp);
+  PRINTF("dBm\r\n");
+	
+	PRINTF("SNR: ");
+	sprintf( temp, "%d", snr);
+	PRINTF(temp);
+  PRINTF("dB\r\n");
+
+	PRINTF("Received -- ");
+	sprintf( temp, "%d", size);
+	PRINTF(temp);
+  PRINTF(" -- bytes.\r\n");
+
+	PRINTF("Packet Contents: ");
+	PRINTF((char *)payload);
+  PRINTF(" .\r\n");
+
+  //if(state == 1) SendPing();
+}
+
+void OnRxError() {
+  PRINTF("RX Error Detected.\r\n");
+}
+
+void rf_init_lora() {
+  radio_events.TxDone = OnTxDone;
+  radio_events.RxDone = OnRxDone;
+  //radio_events.TxTimeout = OnTxTimeout;
+  //radio_events.RxTimeout = OnRxTimeout;
+  radio_events.RxError = OnRxError;
+
+  sx1276_init(radio_events);
+	P4OUT |= BIT1;
+  sx1276_set_channel(RF_FREQUENCY);
+
+	P4OUT &= ~BIT1;
+	__delay_cycles(100);
+	P4OUT |= BIT1;
+
+  sx1276_set_txconfig(MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                                  LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                                  LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                                  true, 0, 0, LORA_IQ_INVERSION_ON, 2000);
+
+	P4OUT &= ~BIT1;
+	__delay_cycles(100);
+	P4OUT |= BIT1;
+
+  sx1276_set_rxconfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                                  LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                                  LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                                  0, true, 0, 0, LORA_IQ_INVERSION_ON, true);
+
+}
+
+int main(void) {
+ 
+	PM5CTL0 &= ~LOCKLPM5;
+
+	// Indicate start of program
+	P8DIR |= BIT1;
+	P8OUT |= BIT1; 
+
+	mcu_init();
+
+	P3SEL0 &= ~BIT4;
+	P3SEL1 |= BIT4;
+	P3DIR |= BIT4;
+
+	P4DIR |= BIT1; 	 // To demarcate sections of the program
+	P4OUT &= ~BIT1;
+
+	INIT_CONSOLE();
+
+  PRINTF("Starting the receiver.\r\n");
+  spi_init();
+
+	//P4OUT |= BIT1;
+	rf_init_lora();
+	P4OUT &= ~BIT1;
+
+	P4OUT |= BIT1;
+	sx1276_set_rx( 0 );
+	P4OUT &= ~BIT1;
+
+	while(1){
+		irq_flag = 0;
+		
+		__bis_SR_register(LPM4_bits+GIE);
+
+		while(irq_flag != 1);
+
+		P4OUT |= BIT1;
+		sx1276_on_dio0irq();
+		P4OUT &= ~BIT1;
+
+	}
+
+	P8OUT &= ~BIT1;
+
+}
