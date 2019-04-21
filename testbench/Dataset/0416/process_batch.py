30ebf0ba001fdf005d43d1e769d67005e3e621d0
Harsh Desai <harshd@andrew.cmu.edu>
2019-04-21 16:54:50 +0100

    Modified batch process file. Removed bmp files, can generate from txt using script when needed

--- testbench/Dataset/0416/process_batch.py
+++ testbench/Dataset/0416/process_batch.py
@@ -8,26 +8,28 @@ w, h = 160, 120
 
 def main():
 
-	for file in os.listdir(directory):
-		filename = os.fsdecode(file)
-		if filename.endswith(".txt"):
-			print(str(filename))
-			f = open(str(cwd + "/" + filename), 'r')
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".txt"):
+            print(str(filename))
+            f = open(str(cwd + "/" + filename), 'r')
 
-			string = f.read()
-		
-			str_split = string.split(' ')
-			str_split = list(map(int, str_split))
+            string = f.read()
 
-			data = np.zeros((h, w), dtype=np.uint8)
+            str_split = string.split(' ')
+            str_split = list(map(int, str_split))
 
-			for y in range(0, h):
-				for x in range(0, w):
-					data[y][x] = str_split[((w) * y) + (x)]
+            data = np.zeros((h, w), dtype=np.uint8)
 
-			img = Image.fromarray(data, 'L') #L = 8bit pixel B/W
-			img = img.rotate(180)
-			img.save(str(cwd + "/" + str(filename) + ".bmp"))
+            for y in range(0, h):
+                    for x in range(0, w):
+                            data[y][x] = str_split[((w) * y) + (x)]
+
+            output_file = filename.split('.')
+            img = Image.fromarray(data, 'L') #L = 8bit pixel B/W
+            img = img.rotate(180)
+            print("Output file: " + str(cwd + "/" + str(output_file[0]) + ".bmp") )
+            img.save(str(cwd + "/" + str(output_file[0]) + ".bmp"))
 
 if __name__ == '__main__':
   main()
