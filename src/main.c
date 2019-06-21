2efcfd642dad4ec4148983b681bc77b2e51eee25
Matteo <nrdmtt@gmail.com>
2019-06-21 14:06:23 -0400

    Move to single branch pins definition

--- src/main.c
+++ src/main.c
@@ -22,6 +22,7 @@
   #include <libio/console.h>
 #endif
 
+//#define old_pins
 
 #define RF_FREQUENCY   915000000 // Hz
 
@@ -95,8 +96,14 @@ int main(void) {
 
 	mcu_init();
 
+#ifdef old_pins
+	P4OUT &= ~BIT7;			// Power to Radio
+	P4DIR |= BIT7;
+#else
 	P4OUT &= ~BIT4;			// Power to Radio
 	P4DIR |= BIT4;
+#endif
+
 
 #ifdef enable_debug
 		INIT_CONSOLE();
@@ -220,7 +227,12 @@ int main(void) {
         PRINTF("Cap ready\n\r");
 #endif  
 
-		P4OUT |= BIT4;
+#ifdef old_pins
+	P4OUT |= BIT7;
+#else
+	P4OUT |= BIT4;
+#endif
+
 		spi_init();
 
 		P8OUT |= BIT2;
@@ -261,7 +273,13 @@ int main(void) {
 		P5SEL1 &= ~(BIT0+ BIT1 + BIT2 + BIT3);
 		P5SEL0 &= ~(BIT0+ BIT1 + BIT2 + BIT3);
 		P5DIR &= ~(BIT0+ BIT1 + BIT2 + BIT3);
-		P4OUT &= ~BIT4;
+
+#ifdef old_pins
+	P4OUT &= ~BIT7;
+#else
+	P4OUT &= ~BIT4;
+#endif
+
 
 	}
 
@@ -324,7 +342,13 @@ void wait_for_charge(){
     //Configure ADC
     ADC12CTL0 = ADC12SHT0_2 | ADC12ON;                      // Sampling time, S&H=4, ADC12 on
     ADC12CTL1 = ADC12SHP | ADC12SHS_1 | ADC12CONSEQ_2;      // Use TA0.1 to trigger, and repeated-single-channel
+
+#ifdef old_pins
+    ADC12MCTL0 = ADC12INCH_7 | ADC12EOS | ADC12WINC;        // A7 ADC input select; Vref+ = AVCC
+#else
     ADC12MCTL0 = ADC12INCH_5 | ADC12EOS | ADC12WINC;        // A7 ADC input select; Vref+ = AVCC
+#endif
+
     ADC12HI = High_Threshold;                               // Enable ADC interrupt
     ADC12IER2 = ADC12HIIE;                                  // Enable ADC threshold interrupt
     ADC12CTL0 |= ADC12ENC | ADC12SC;                        // Start sampling/conversion
@@ -422,9 +446,6 @@ void __attribute__ ((interrupt(ADC12_B_VECTOR))) ADC12ISR (void){
 
     switch(__even_in_range(ADC12IV, ADC12IV__ADC12RDYIFG))
     {
-        case ADC12IV__NONE:        break;   // Vector  0:  No interrupt
-        case ADC12IV__ADC12OVIFG:  break;   // Vector  2:  ADC12MEMx Overflow
-        case ADC12IV__ADC12TOVIFG: break;   // Vector  4:  Conversion time overflow
         case ADC12IV__ADC12HIIFG:  		    // Vector  6:  ADC12BHI
         ADC12IER2 = ~(ADC12HIIE);
        	__bic_SR_register_on_exit(LPM3_bits | GIE);
@@ -434,38 +455,6 @@ void __attribute__ ((interrupt(ADC12_B_VECTOR))) ADC12ISR (void){
         case ADC12IV__ADC12IFG0:            // Vector 12:  ADC12MEM0 Interrupt
 			//PRINTF("ADC: %i\n\r", ADC12MEM0);
             break;
-        case ADC12IV__ADC12IFG1:   break;   // Vector 14:  ADC12MEM1
-        case ADC12IV__ADC12IFG2:   break;   // Vector 16:  ADC12MEM2
-        case ADC12IV__ADC12IFG3:   break;   // Vector 18:  ADC12MEM3
-        case ADC12IV__ADC12IFG4:   break;   // Vector 20:  ADC12MEM4
-        case ADC12IV__ADC12IFG5:   break;   // Vector 22:  ADC12MEM5
-        case ADC12IV__ADC12IFG6:   break;   // Vector 24:  ADC12MEM6
-        case ADC12IV__ADC12IFG7:   break;   // Vector 26:  ADC12MEM7
-        case ADC12IV__ADC12IFG8:   break;   // Vector 28:  ADC12MEM8
-        case ADC12IV__ADC12IFG9:   break;   // Vector 30:  ADC12MEM9
-        case ADC12IV__ADC12IFG10:  break;   // Vector 32:  ADC12MEM10
-        case ADC12IV__ADC12IFG11:  break;   // Vector 34:  ADC12MEM11
-        case ADC12IV__ADC12IFG12:  break;   // Vector 36:  ADC12MEM12
-        case ADC12IV__ADC12IFG13:  break;   // Vector 38:  ADC12MEM13
-        case ADC12IV__ADC12IFG14:  break;   // Vector 40:  ADC12MEM14
-        case ADC12IV__ADC12IFG15:  break;   // Vector 42:  ADC12MEM15
-        case ADC12IV__ADC12IFG16:  break;   // Vector 44:  ADC12MEM16
-        case ADC12IV__ADC12IFG17:  break;   // Vector 46:  ADC12MEM17
-        case ADC12IV__ADC12IFG18:  break;   // Vector 48:  ADC12MEM18
-        case ADC12IV__ADC12IFG19:  break;   // Vector 50:  ADC12MEM19
-        case ADC12IV__ADC12IFG20:  break;   // Vector 52:  ADC12MEM20
-        case ADC12IV__ADC12IFG21:  break;   // Vector 54:  ADC12MEM21
-        case ADC12IV__ADC12IFG22:  break;   // Vector 56:  ADC12MEM22
-        case ADC12IV__ADC12IFG23:  break;   // Vector 58:  ADC12MEM23
-        case ADC12IV__ADC12IFG24:  break;   // Vector 60:  ADC12MEM24
-        case ADC12IV__ADC12IFG25:  break;   // Vector 62:  ADC12MEM25
-        case ADC12IV__ADC12IFG26:  break;   // Vector 64:  ADC12MEM26
-        case ADC12IV__ADC12IFG27:  break;   // Vector 66:  ADC12MEM27
-        case ADC12IV__ADC12IFG28:  break;   // Vector 68:  ADC12MEM28
-        case ADC12IV__ADC12IFG29:  break;   // Vector 70:  ADC12MEM29
-        case ADC12IV__ADC12IFG30:  break;   // Vector 72:  ADC12MEM30
-        case ADC12IV__ADC12IFG31:  break;   // Vector 74:  ADC12MEM31
-        case ADC12IV__ADC12RDYIFG: break;   // Vector 76:  ADC12RDY
         default: break;
     }
 }
