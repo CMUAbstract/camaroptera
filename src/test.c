01c28c290379711205e047c11f361fecadf95588
desaiharsh <harshd@andrew.cmu.edu>
2019-09-28 18:45:38 -0400

    Added radio stuff to test code

--- src/test.c
+++ src/test.c
@@ -10,10 +10,43 @@
 
 #include <libmspdriver/aes256.h>
 
+#include <liblora/spi.h>
+#include <liblora/sx1276.h>
+#include <liblora/sx1276regs-fsk.h>
+#include <liblora/sx1276regs-lora.h>
+
+#define RF_FREQUENCY   915000000 // Hz
+
+#define FSK_FDEV                          25e3      // Hz
+#define FSK_DATARATE                      50e3      // bps
+#define FSK_BANDWIDTH                     50e3      // Hz
+#define FSK_AFC_BANDWIDTH                 83.333e3  // Hz
+#define FSK_PREAMBLE_LENGTH               5         // Same for Tx and Rx
+#define FSK_FIX_LENGTH_PAYLOAD_ON         false
+
+#define LORA_BANDWIDTH                              2        // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
+#define LORA_SPREADING_FACTOR                       7        // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         5         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+#define RX_TIMEOUT_VALUE                  3500
+#define TX_OUTPUT_POWER										17        // dBm
+#define BUFFER_SIZE                       256 // Define the payload size here
+
+#define __ro_hifram __attribute__((section(".upper.rodata")))
+
+void camaroptera_init_lora();
+void OnTxDone();
+
 uint16_t AES_baseAddr = 0x09C0;
-const uint8_t encryptionKey[4] = {0x01, 0x02, 0x03, 0x04};
+const uint8_t encryptionKey[16] = { 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04 };
 const uint8_t input_data[16] = { 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04 };
 uint8_t output_data[16];
+
+__ro_hifram static radio_events_t radio_events;
 
 void main(){
 	msp_watchdog_disable();
@@ -27,6 +60,33 @@ void main(){
 	AES256_encryptData( AES_baseAddr, input_data, output_data );
 	
 	for( int i = 0; i < 16; i++ )
-		PRINTF("Byte %i: %i\r\n", i, output_data[i]);
+		PRINTF("Byte %i: %x\r\n", i, output_data[i]);
+				
+	spi_init();
+	camaroptera_init_lora();
+	//sx1276_send( radio_buffer,  last_packet_size);
 	
 }
+
+void OnTxDone() {
+	//tx_packet_index ++;
+	//sent_packet_count ++;
+	//frame_track += 253;
+}
+
+void camaroptera_init_lora() {
+  radio_events.TxDone = OnTxDone;
+  //radio_events.RxDone = OnRxDone;
+  //radio_events.TxTimeout = OnTxTimeout;
+  //radio_events.RxTimeout = OnRxTimeout;
+  //radio_events.RxError = OnRxError;
+
+  sx1276_init(radio_events);
+  sx1276_set_channel(RF_FREQUENCY);
+
+	sx1276_set_txconfig(MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                                  LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                                  LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                                  true, 0, 0, LORA_IQ_INVERSION_ON, 2000);
+}
+
