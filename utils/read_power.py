dbb7a8243e858f817ab91a3f69bf74f12fa76f36
penguin2207 <eli@rho.net>
2023-04-06 19:29:50 -0400

    More minor changes

--- utils/read_power.py
+++ utils/read_power.py
@@ -7,7 +7,7 @@ import argparse
 import numpy as np
 import math
 
-VCC = 3.3 								#3.3 for serial/usb power, 3 o/w
+VCC = 3.2726 								#3.3 for serial/usb power, 3 o/w
 ADC_BIT_DEPTH = 12 						#Using ADC12
 ADC_RANGE = math.pow(2,ADC_BIT_DEPTH)
 CAL_TIME = 0.996 						#Calibrate using bit flip script/saleae (ms)
@@ -17,6 +17,8 @@ READ_INTERVAL = 10 						#Interval must be an integer, reading taken every READ_
 
 def main(args):
 	samp_count = 0
+	last_data = 0
+	power = 0
 
 	print(f'[DEBUG] Starting Power Read')
 
@@ -25,12 +27,15 @@ def main(args):
 			data = ser.readline().decode('utf-8').replace("\r\n", "")
 			if data.isdigit():
 				conv_data = (int(data) * VCC)/ADC_RANGE
+				if(samp_count!=0):
+					power = ((conv_data - last_data) / (CAL_TIME/1000) ) 
 				if(samp_count % READ_INTERVAL == 0):
 					with open(args.file, 'a') as file:
 						writer = csv.writer(file, delimiter = '\t')
      
-						writer.writerow([round(samp_count*CAL_TIME,3), round(conv_data,3)])
+						writer.writerow([round(samp_count*CAL_TIME,3), round(conv_data,3), round(power, 3)])
 						file.close()
+				last_data = conv_data
 				samp_count += 1
 
 if __name__ == '__main__':
